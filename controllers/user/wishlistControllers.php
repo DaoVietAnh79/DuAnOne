3327960fa4fa0c8d8724cbd58334f84506c511cc
Viet17032004 <viethmph39977@fpt.edu.vn>
2024-08-07 12:30:20 +0700

    fix gio hang

--- controllers/user/wishlistControllers.php
+++ controllers/user/wishlistControllers.php
@@ -2,5 +2,7 @@
 if(isset($_GET['del'])){
   array_splice($_SESSION['cart'], $_GET['del'], 1);
   $_SESSION['cart'] = array_values($_SESSION['cart']);
+}else{
+  
 }
 include_once("views/user/wishlist.views.php");
